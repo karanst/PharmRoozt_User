9bdd2a047a8e845aa3999199e976b959b0f0bc4c
alphamac <karanstomar@icloud.com>
2023-02-03 18:36:45 +0530

    Initial commit

---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,21 @@
+final String appName = 'PharmRootz';
+
+final String packageName = 'com.pharmrootz.user';
+final String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+final String iosPackage = 'com.pharmrootz.user';
+final String iosLink = 'your ios link here';
+final String appStoreId = 'com.pharmrootz.user';
+
+final String deepLinkUrlPrefix = 'https://alpha.ecommerce.link';
+final String deepLinkName = 'alpha.ecommerce.link';
+
+final int timeOut = 50;
+const int perPage = 10;
+
+// final String baseUrl = 'https://developmentalphawizz.com/zuqzuq/app/v1/api/';
+
+final String baseUrl = 'https://pharmrootz.com/app/v1/api/';
+final String imageUrl = 'https://pharmrootz.com/';
+
+final String jwtKey = "e1e421dceee9ea718ab764ea4fdef95629e7f415";
